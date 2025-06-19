0a47f160dc1f681e1cfabc54f6a565154b37a741
turtleYJ <yooil605@gmail.com>
2025-06-19 10:22:39 +0900

    feat: docker-compose & project init

---
+++ elsdemo/src/main/java/com/y1/elsdemo/ElsdemoApplication.java
@@ -0,0 +1,13 @@
+package com.y1.elsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElsdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElsdemoApplication.class, args);
+	}
+
+}
---
+++ elsdemo/src/test/java/com/y1/elsdemo/ElsdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.y1.elsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElsdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
