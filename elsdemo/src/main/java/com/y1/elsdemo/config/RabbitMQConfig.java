f135e351b956aa639984deda62ef125e3a88475c
turtleYJ <yooil605@gmail.com>
2025-06-24 20:02:48 +0900

    feat: MQ config 코드 작성

--- elsdemo/src/main/java/com/y1/elsdemo/config/RabbitMQConfig.java
+++ elsdemo/src/main/java/com/y1/elsdemo/config/RabbitMQConfig.java
@@ -1,2 +1,16 @@
-package com.y1.elsdemo.config;public class RabbitMQConfig {
+package com.y1.elsdemo.config;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    public static final String MAIL_QUEUE = "mail-queue";
+
+    @Bean
+    public Queue mailQueue() {
+        return new Queue(MAIL_QUEUE, true);
+    }
 }
