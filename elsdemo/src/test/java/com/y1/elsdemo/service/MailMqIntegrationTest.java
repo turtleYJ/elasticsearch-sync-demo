d486bd975902f61d9279e79c964cb38a38b88873
turtleYJ <yooil605@gmail.com>
2025-06-27 18:38:44 +0900

    fix: MailMqIntegraionTest 테스트 코드 수정
    * Instant 직렬화를 위한 jackson-datatype-jsr310 모듈 추가

--- elsdemo/src/test/java/com/y1/elsdemo/service/MailMqIntegrationTest.java
+++ elsdemo/src/test/java/com/y1/elsdemo/service/MailMqIntegrationTest.java
@@ -1,5 +1,7 @@
 package com.y1.elsdemo.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.y1.elsdemo.model.MailDocument;
 import org.junit.jupiter.api.Test;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
@@ -33,8 +35,14 @@ public class MailMqIntegrationTest {
                 Instant.now()
         );
 
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+
+        // Json 문자열로 변환
+        String json = objectMapper.writeValueAsString(mail);
+
         // when: mq publish
-        rabbitTemplate.convertAndSend(QUEUE_NAME, mail);
+        rabbitTemplate.convertAndSend(QUEUE_NAME, json);
 
         // then: 비동기라서 잠깐 대기 (리스너가 ES에 저장할 때까지)
         Thread.sleep(1000);
